07f246925cf5892e7786cdd8fa329de5fcbcee14
Adrian40 <adrian.balogh@outlook.com>
2017-03-10 13:03:50 +0100

    MVC done

---
+++ CustomerOrderApplication/CustomerOrderApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerOrderApplication.Startup))]
+namespace CustomerOrderApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
